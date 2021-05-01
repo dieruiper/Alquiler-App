36af483a5dad5f8333dd75cb1fd24bf29a035889
antcoresp <antoniocorpas97@gmail.com>
2021-05-01 13:54:12 +0200

    pantalla registro reserva hecha

--- app/src/main/java/com/example/mapaestaciones/RegistroUsuario.java
+++ app/src/main/java/com/example/mapaestaciones/RegistroUsuario.java
@@ -2,37 +2,38 @@ package com.example.mapaestaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.util.Calendar;
-import java.util.Date;
 
-public class FormularioResumen extends AppCompatActivity {
+public class RegistroUsuario extends AppCompatActivity {
     EditText formres_nombre,formres_apellidos,formres_correo,formres_fecha,formres_comentarios;
+    Button bfechaRegistro;
     private int dia,mes,ano;
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_formulario_resumen);
+        setContentView(R.layout.activity_registro_usuario);
         formres_nombre=(EditText) findViewById(R.id.formres_nombre);
         formres_apellidos =(EditText) findViewById(R.id.formres_apellidos);
+        //----------------¿HARÁ FILTRO DE FORMATO DE CORREO NO VÁLIDO?--------------
         formres_correo=(EditText) findViewById(R.id.formres_correo);
+        //----------------FILTRO DE MAYOR DE 18--------------
         formres_fecha=(EditText) findViewById(R.id.formres_fecha);
-        formres_comentarios= (EditText) findViewById(R.id.formres_comentarios);
+        bfechaRegistro=(Button)findViewById(R.id.bfechaRegistro);
+        //formres_comentarios= (EditText) findViewById(R.id.formres_comentarios);
 
 
     }
     public void onClick(View v) {
 
-        if (v == formres_fecha) {
+        if (v == bfechaRegistro) {
             final Calendar c = Calendar.getInstance();
             dia = c.get(Calendar.DAY_OF_MONTH);
             mes = c.get(Calendar.MONTH);
@@ -40,7 +41,7 @@ public class FormularioResumen extends AppCompatActivity {
             DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
                 @Override
                 public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                    formres_fecha.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    bfechaRegistro.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
 
                 }
             }
@@ -50,8 +51,11 @@ public class FormularioResumen extends AppCompatActivity {
     }
     public void volver_reserva_vehiculos(View view){
 
-        //Intent volver_reserva_vehiculos = new Intent(this,Activity_Reservar_Vehiculos.class );
-        //startActivity(volver_reserva_vehiculos);
-        finish();
+        Intent volver_reserva_vehiculos = new Intent(this,Activity_Reservar_Vehiculos.class );
+        //if(formres_fecha.compa)
+        startActivity(volver_reserva_vehiculos);
+
+
+        //finish();
     }
 }
