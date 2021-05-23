dbab1f4a4714a9c0a36193d73b73e77cbb25a860
dieruiper <47790082+dieruiper@users.noreply.github.com>
2021-05-24 00:46:54 +0200

    pasar actividad reserva

--- app/src/main/java/com/example/mapaestaciones/Activity_Confirmar_Reserva.java
+++ app/src/main/java/com/example/mapaestaciones/Activity_Confirmar_Reserva.java
@@ -2,13 +2,28 @@ package com.example.mapaestaciones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity_Confirmar_Reserva extends AppCompatActivity {
-
+    String efecha_resumen,efecha2_resumen,elugar_resumen;
+    TextView tv_lugar_recogida,tv_lugar_entrega,tv_fecha_recogida,tv_fecha_entrega;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__confirmar__reserva);
+
+
+        Intent me=getIntent();
+        efecha_resumen=me.getStringExtra("es_efecha");
+        efecha2_resumen=me.getStringExtra("es_efecha2");
+        elugar_resumen=me.getStringExtra("es_elugar");
+
+        tv_lugar_recogida.setText(elugar_resumen);
+        tv_fecha_recogida=findViewById(R.id.fecha_recogida_resumen);
+        tv_fecha_recogida.setText(efecha_resumen);
+        tv_fecha_entrega=findViewById(R.id.efecha2_resumen);
+        tv_fecha_entrega.setText(efecha2_resumen);
     }
 }
--- app/src/main/java/com/example/mapaestaciones/Activity_Seleccionar_Vehiculo.java
+++ app/src/main/java/com/example/mapaestaciones/Activity_Seleccionar_Vehiculo.java
@@ -20,6 +20,7 @@ public class Activity_Seleccionar_Vehiculo extends AppCompatActivity {
 
     private VehiculoAdapter vAdapter;
     TextView tv_lugar_recogida,tv_lugar_entrega,tv_fecha_recogida,tv_fecha_entrega;
+    String efecha_resumen,efecha2_resumen,elugar_resumen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +28,13 @@ public class Activity_Seleccionar_Vehiculo extends AppCompatActivity {
         setContentView(R.layout.activity_seleccionar_vehiculo);
 
         Intent me=getIntent();
-        String efecha_resumen=me.getStringExtra("es_efecha");
-        String efecha2_resumen=me.getStringExtra("es_efecha2");
-        String elugar_resumen=me.getStringExtra("es_elugar");
-        String elugar2_resumen=me.getStringExtra("es_elugar2");
+        efecha_resumen=me.getStringExtra("es_efecha");
+        efecha2_resumen=me.getStringExtra("es_efecha2");
+        elugar_resumen=me.getStringExtra("es_elugar");
+        //String elugar2_resumen=me.getStringExtra("es_elugar2");
 
-        tv_lugar_entrega = findViewById(R.id.elugar_recogida_resumen);
-        tv_lugar_entrega.setText(elugar2_resumen);
+        //tv_lugar_entrega = findViewById(R.id.elugar_recogida_resumen);
+        //tv_lugar_entrega.setText(elugar2_resumen);
         tv_lugar_recogida = findViewById(R.id.elugar_devolucion_resumen);
         tv_lugar_recogida.setText(elugar_resumen);
         tv_fecha_recogida=findViewById(R.id.fecha_recogida_resumen);
@@ -74,6 +75,14 @@ public class Activity_Seleccionar_Vehiculo extends AppCompatActivity {
 
     public void confirmar_Reserva(View view){
         Intent i = new Intent(this, Activity_Confirmar_Reserva.class );
+        String c_efecha =efecha_resumen;
+        String c_efecha2 =efecha2_resumen;
+        String c_elugar = elugar_resumen;
+        //String s_elugar2 = et_lugar_recogida.getText().toString().trim();
+
+        i.putExtra("es_efecha", c_efecha);
+        i.putExtra("es_efecha2",  c_efecha2);
+        i.putExtra("es_elugar",  c_elugar);
         startActivity(i);
     }
 
